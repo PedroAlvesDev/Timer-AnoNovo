bcf4aa94c8e37cd276a187a1082c1e28b33ce810
Seu Nome <139656791+PedroAlvesDev@users.noreply.github.com>
2024-12-31 22:10:24 -0300

    addLeadingZero

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const timerElement = document.getElementById('timer');
 const nextYear = new Date().getFullYear() + 1;
 const newYearDate = new Date(`January 01, ${nextYear}, 00:00:00`);
 
+function addLeadingZero(number) {
+    return number < 10 ? `0${number}` : number;
+};
+
 function updateTimer() {
     const now = new Date();
     const timeRemaining = newYearDate - now;
@@ -18,11 +22,10 @@ function updateTimer() {
     const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-    timerElement.textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    timerElement.textContent = `${addLeadingZero(days)}d ${addLeadingZero(hours)}h ${addLeadingZero(minutes)}m ${addLeadingZero(seconds)}s`;
 };
 
 const timerInterval = setInterval(updateTimer, 1000);
 
-uodateTimer();
-
+updateTimer();
 
